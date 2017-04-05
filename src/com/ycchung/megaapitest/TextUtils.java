15e73720b7c0a8bc2455c8e7f259218d8c3bc445
ycchung <b51235423@gmail.com>
2017-04-05 11:47:08 +0800

    Update TextUtils.java

--- src/com/ycchung/megaapitest/TextUtils.java
+++ src/com/ycchung/megaapitest/TextUtils.java
@@ -3,6 +3,9 @@ package com.ycchung.megaapitest;
 /**
  * Created by ycchung on 2017/3/23.
  */
+
+// Please remove this class when in Android
+
 public class TextUtils {
     public static boolean isEmpty(CharSequence str) {
         if (str == null || str.length() == 0)
